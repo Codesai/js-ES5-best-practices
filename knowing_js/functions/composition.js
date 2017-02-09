be8e15188ed08d7f44c08a74af3b54d6980ed25b
Jose Manuel Deniz Suarez <josedeniz@Joses-MacBook-Pro.local>
2017-02-09 17:55:38 +0000

    Add composition.js

---
+++ knowing_js/functions/composition.js
@@ -0,0 +1,39 @@
+var cook = function(food) {
+    console.log("Cook " + food + "!");
+    return food;
+};
+
+var eat = function(food) {
+    console.log("Eat " + food + "!");
+    return food;
+};
+
+var cookAndEat = function(food) {
+    eat(cook(food));
+    return food;
+};
+
+cookAndEat("chocolate");
+
+var compose = function(f, g) {
+    return function(c) {
+        return f(g(c));
+    };
+};
+
+var cookAndEat = compose(eat, cook);
+
+cookAndEat("chocolate");
+
+var dreamWith = function(food) {
+    console.log("Dream with " + food + "!");
+    return food;
+};
+
+var identity = function(x) {
+    return x;
+};
+
+var fn = [eat, cook, dreamWith].reduce(compose, identity);
+
+fn("Cookies");
