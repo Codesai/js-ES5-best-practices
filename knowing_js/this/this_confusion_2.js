22f9c448cfc5de96e2963ea710278666168df2b7
trikitrok <trikitrok@gmail.com>
2017-02-10 17:01:54 +0100

    adding another example

---
+++ knowing_js/this/this_confusion_2.js
@@ -0,0 +1,17 @@
+/// Typical confusion: assuming this refers to the function itself
+
+function foo() {
+  var a = 2;
+  this.bar();
+}
+
+function bar() {
+  console.log( "bar was called" );  
+  console.log( this.a );
+  console.log( "*****************" ); 
+}
+
+foo();
+
+/// Example from YDNJS: this & Object Prototypes chapter 1
+/// https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch1.md
