04ac378acc34e1676a3dca50c692738b515f5ead
trikitrok <trikitrok@gmail.com>
2017-02-11 19:16:00 +0100

    adding implicit binding with a property reference chain example

---
+++ knowing_js/this/implicit_binding_property_reference_chain.js
@@ -0,0 +1,20 @@
+// Implicit binding with a property reference chain
+
+function foo() {
+    console.log( this.a );
+}
+
+var obj2 = {
+    a: 42,
+    foo: foo
+};
+
+var obj1 = {
+    a: 2,
+    obj2: obj2
+};
+
+obj1.obj2.foo();
+
+/// Example from YDNJS: this & Object Prototypes chapter 2
+/// https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch2.md
