c414795ea3a030c00a06fba518e851f01844cd62
trikitrok <trikitrok@gmail.com>
2017-02-11 19:28:46 +0100

    adding loosing this binding example

---
+++ knowing_js/this/loosing_binding.js
@@ -0,0 +1,19 @@
+// Loosing this binding
+
+function foo() {
+  console.log( this.a );
+}
+
+var obj = {
+  a: 2,
+  foo: foo
+};
+
+var bar = obj.foo;
+
+var a = "oops, global";
+
+bar();
+
+/// Example from YDNJS: this & Object Prototypes chapter 2
+/// https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch2.md
