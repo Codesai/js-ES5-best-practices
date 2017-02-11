b27787acb56fd27e37b831f875a52c852659f81e
trikitrok <trikitrok@gmail.com>
2017-02-11 19:08:32 +0100

    adding implicit binding example

---
+++ knowing_js/this/implicit_binding.js
@@ -0,0 +1,15 @@
+// Implicit binding
+
+function foo() {
+  console.log( this.a );
+}
+
+var obj = {
+  a: 2,
+  foo: foo
+};
+
+obj.foo();
+
+/// Example from YDNJS: this & Object Prototypes chapter 2
+/// https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch2.md
