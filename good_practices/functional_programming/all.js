b84c3888c63b5dc83416b499ec339ebd46bfa9c6
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2017-02-13 17:17:50 +0000

    Add lodash all example

--- good_practices/functional_programming/all.js
+++ good_practices/functional_programming/all.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 function even(number) {
     return number % 2 === 0;
 }
@@ -31,3 +33,10 @@ function allOdd(numbers) {
     }
     return true;
 }
+
+allEven([1, 2, 3, 4]);
+allOdd([1, 2, 3, 4]);
+
+// Using lodash
+_.every([2, 4], even);
+_.every([1, 3], odd);
