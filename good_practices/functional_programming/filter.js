1166659bc02a967db68a870d74023796bd289b92
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2017-02-13 13:59:22 +0000

    Add filter.js

---
+++ good_practices/functional_programming/filter.js
@@ -0,0 +1,44 @@
+var getOdds = function(numbers) {
+    var i,
+        res = [];
+    for (i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 !== 0) {
+            res.push(numbers[i]);
+        }
+    }
+    return res;
+};
+
+var getNegatives = function(numbers) {
+    var i,
+        res = [];
+    for (i = 0; i < numbers.length; i++) {
+        if (numbers[i] < 0) {
+            res.push(numbers[i]);
+        }
+    }
+    return res;
+};
+
+var filter = function(pred, values) {
+    var i,
+        res = [];
+    for (i = 0; i < values.length; i++) {
+        if (pred(values[i])) {
+            res.push(values[i]);
+        }
+    }
+    return res;
+};
+
+filter(function(num) {
+    return num % 2 === 0;
+}, [1, 2, 3, 4, 5, 6])
+
+filter(function(num) {
+    return num % 2 === 1;
+}, [1, 2, 3, 4, 5, 6])
+
+filter(function(num) {
+    return num > 0;
+}, [-1, -2, -3, 4, -5, 6])
