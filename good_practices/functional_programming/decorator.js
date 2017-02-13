81f059e737c7087260a4681230129a13d685ec1f
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2017-02-13 14:29:54 +0000

    Add decorator.js

---
+++ good_practices/functional_programming/decorator.js
@@ -0,0 +1,32 @@
+function complement(fn) {
+    return function() {
+        return !fn.apply(this, arguments);
+    };
+}
+
+var equal = function(a, b) { return a === b; };
+
+var notEqual = complement(equal);
+
+function maybe(fn) {
+    return function() {
+        var i;
+
+        if (arguments.length === 0) {
+            return;
+        } else {
+            for (i = 0; i < arguments.length; ++i) {
+                if (arguments[i] == null) {
+                    return;
+                }
+            }
+            return fn.apply(this, arguments);
+        }
+    };
+}
+
+function Model() {};
+
+Model.prototype.setSomething = maybe(function(value) {
+    this.something = value;
+});
