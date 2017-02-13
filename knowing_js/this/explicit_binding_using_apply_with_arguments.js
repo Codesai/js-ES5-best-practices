7d233f0227843334d1aef677cb032facd42edfcc
trikitrok <trikitrok@gmail.com>
2017-02-13 17:00:48 +0100

    adding some examples of explicit binding with call

---
+++ knowing_js/this/explicit_binding_using_apply_with_arguments.js
@@ -0,0 +1,18 @@
+// Explicit binding using call and passing more arguments
+
+function print(num, random) {
+  console.log(
+    '#' + num + ' ' + this.species + ': ' + this.name + ' ' + random
+  );
+}
+
+var animals = [
+  { species: 'Lion', name: 'King' },
+  { species: 'Whale', name: 'Moby dick' }
+];
+
+for (var i = 0; i < animals.length; i++) {
+  print.call(animals[i], i, Math.random());
+}
+
+// Example modified from example in https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
