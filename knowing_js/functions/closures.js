3a86c880eae6531e744fd049a0682e624bbd06f3
trikitrok <trikitrok@gmail.com>
2017-03-21 23:47:36 +0100

    improving example

--- knowing_js/functions/closures.js
+++ knowing_js/functions/closures.js
@@ -5,3 +5,13 @@
         return x * y;
     };
 })(3)(4);
+
+var foo = (function(x) {
+    // env -> {x: 3, '..': {}}
+    return function(y) {
+        // env ->{y: 4, '..': {x: 3, '..': {}}}
+        return x * y;
+    };
+})(3);
+
+foo(4);
